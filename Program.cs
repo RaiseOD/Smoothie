ab34b319472e3a80f2506c3c040bee1d584673de
RaiseOD <75505270+RaiseOD@users.noreply.github.com>
2021-09-28 14:27:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Ryan von Lutzow
+// Smoothie Extra Credit
+// 9-28-21
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+using Smoothie;
+
+namespace Smoothie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> IngredientNames = new List<string>();
+            List<double> IngredientPrices = new List<double>();
+
+            Smoothie s = new Smoothie(IngredientNames, IngredientPrices);
+
+            s.IngredientNames.Add("Strawberry");
+            s.IngredientNames.Add("Banana");
+            s.IngredientNames.Add("Mango");
+            s.IngredientNames.Add("Blueberry");
+            s.IngredientNames.Add("Raspberry");
+            s.IngredientNames.Add("Apple");
+            s.IngredientNames.Add("Pineapple");
+            s.IngredientPrices.Add(1.50);
+            s.IngredientPrices.Add(0.50);
+            s.IngredientPrices.Add(2.50);
+            s.IngredientPrices.Add(1.00);
+            s.IngredientPrices.Add(1.00);
+            s.IngredientPrices.Add(1.75);
+            s.IngredientPrices.Add(3.50);
+
+            s.GetName();
+
+
+        }
+
+    }
+
+}
---
+++ Smoothie.cs
@@ -0,0 +1,70 @@
+﻿// Ryan von Lutzow
+// Smoothie Class
+// 9-28-21
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smoothie
+{
+    class Smoothie
+    {
+
+        public List<string> IngredientNames = new List<string>();
+        public List<double> IngredientPrices = new List<double>();
+
+        public Smoothie(List<string> IngredientNames, List<double> IngredientPrices)
+        {
+            
+            IngredientNames = this.IngredientNames;
+            IngredientPrices = this.IngredientPrices;
+
+        }
+
+        public void GetName()
+        {
+
+            Console.WriteLine("Ingredients: ");
+
+            Console.WriteLine(IngredientNames.Count);
+            
+            if (IngredientNames.Count > 1)
+            {
+
+                for (var i = 0; i < IngredientNames.Count - 1; i++)
+                {
+
+                    Console.Write(IngredientNames[i] + ", ");
+
+                }
+
+                Console.Write(IngredientNames[IngredientNames.Count - 1] + " Fusion ");
+
+            }
+            else
+            {
+
+                Console.Write(IngredientNames[0] + "Smoothie ");
+
+            }
+
+        }
+
+        public void GetCost()
+        {
+
+
+
+        }
+
+        public void GetPrice()
+        {
+
+
+
+        }
+
+    }
+
+}
